2bacda8787b320fc886a77d4bb0c589cf378c8a6
Cody Toney-Griffith <cody.toney@gmail.com>
2020-04-30 14:08:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,19 +16,18 @@ class Song
 #    new_song
 #  end
 def self.new_by_filename(filename)
-    song = self.new
-    song.title = filename.split(" - ")[1]
-    song
+  song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
   end
 def self.all
   @@all
 end
   def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
+    self.artist = Artist.find_or_create_by_name(name)
+      artist.add_song(self)
   end
 
 
