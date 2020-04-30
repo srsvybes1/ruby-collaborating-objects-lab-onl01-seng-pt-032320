23fc7975a1c2825ae5a28ec7acfdf8d85bc46c22
Cody Toney-Griffith <cody.toney@gmail.com>
2020-04-30 13:41:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ class Song
     #artist = file_details[0]
     #song = file_details[1]
     new_song = self.new(song)
-    #new_song.artist_name = artist
+    new_song.artist_name = artist
     #new_song
   end
 def self.all
